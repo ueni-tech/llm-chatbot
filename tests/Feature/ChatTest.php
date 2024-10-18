1bbece13d62b472e98865e6963c188a41933b128
uenishi <ueni.tech@gmail.com>
2024-10-18 18:22:39 +0900

    #3 LoginTest作成

---
+++ tests/Feature/ChatTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ChatTest extends TestCase
+{
+  use RefreshDatabase;
+}
